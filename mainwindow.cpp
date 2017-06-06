227d3d2c87daecfb90bceecf282242f0470c26da
Jar196rus <Jar196rus@gmail.com>
2017-06-06 03:27:59 +0300

    Courswork_without_crypto

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,4 +11,48 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    client_status=0;
+}
+
+void MainWindow::read() {
+    QByteArray datagram;
+    datagram.resize(udpChat->pendingDatagramSize());
+    QHostAddress *address = new QHostAddress();
+    udpChat->readDatagram(datagram.data(), datagram.size(), address);
+
+    QDataStream in(&datagram, QIODevice::ReadOnly);
+
+    // где-то тут алгоритм дешифровки
+
+    qint8 type = 0;
+    in >> type;
+    QString str;
+    in >> str;
+    ui->textinfo->append(str);
+}
+
+void MainWindow::on_connecting_clicked()
+{
+    client_status=1;
+    nickname=ui->Nickbar->toPlainText();
+    udpChat = new QUdpSocket(this);
+    udpChat->bind(QHostAddress::Any, 33334);
+    connect(udpChat, SIGNAL(readyRead()), SLOT(read()));
+}
+
+void MainWindow::on_sending_clicked()
+{
+    if(client_status==1) {
+        QString str=(nickname + " : " + ui->messeger->toPlainText());
+        ui->textinfo->append(str);
+        //тут алгоритм шифрования
+        QByteArray data;
+        QDataStream out(&data, QIODevice::WriteOnly);
+        out << qint64(0);
+        out << str;
+        out.device()->seek(qint64(0));
+        out << qint64(data.size() - sizeof(qint64));
+        udpChat->writeDatagram(data, QHostAddress::Broadcast, 33334);
+    }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,23 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork>
+#include <QUdpSocket>
+#include <QTcpSocket>
+#include <QObject>
+#include <QByteArray>
+#include <QDebug>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
+
+namespace Ui {
+    class MainWindow;
+}
+
+class QTcpServer;
+class QUdpChat;
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +28,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void read();
+    void on_connecting_clicked();
+    void on_sending_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QUdpSocket *udpChat;
+    QTcpServer *tcpServer;
+    int client_status;
+    QString nickname;
+    QMap<int,QTcpSocket *> SClients;
 };
-
 #endif // MAINWINDOW_H
